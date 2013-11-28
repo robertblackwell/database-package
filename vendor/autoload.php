2105b78413fee621d5a76011603a6f949d48bc04
Robert Blackwell <rob@whiteacorn.com>
2013-11-28 18:20:17 +0200

    changing to local repositories only

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit062aaae181839e9e129f3565aba4dca8::getLoader();
+return ComposerAutoloaderInitba43a41b62ce64f039c8f705099b8be7::getLoader();
