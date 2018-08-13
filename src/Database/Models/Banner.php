e9e842ae83330dab67c003fbf40041c902d8c2be
robertblackwell <rob@whiteacorn.com>
2018-08-13 10:54:56 -0700

    debugging

--- src/Database/Models/Banner.php
+++ src/Database/Models/Banner.php
@@ -85,7 +85,7 @@ class Banner extends Base\ModelBase
         $obj = new HEDObject();
         $fn = self::$locator->banner_filepath($trip, $slug);
 
-        print "<p>".__METHOD__." file name : {$fn}</p>";
+        // print "<p>".__METHOD__." file name : {$fn}</p>";
 
 		$images_dir = self::$locator->banner_images_dir($trip, $slug);
         $obj->get_from_file($fn);
